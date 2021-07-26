53e76340d77009d22b8b74405061cc41885f5e7d
Ze Baroni <zerenatobaroni@gmail.com>
2021-07-26 14:30:10 -0300

    askedrasrasr

--- app/Domains/Importacao/Actions/ImportacaoCNAB/ProcessarLinhasDetalhesCnab.php
+++ app/Domains/Importacao/Actions/ImportacaoCNAB/ProcessarLinhasDetalhesCnab.php
@@ -7,7 +7,6 @@ namespace App\Domains\Importacao\Actions\ImportacaoCNAB;
 use App\Domains\Importacao\DTOs\DadosProcessamentoCNAB;
 use App\Domains\Titulo\DTO\TituloDTO;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
 use Spatie\QueueableAction\QueueableAction;
 
 class ProcessarLinhasDetalhesCnab
@@ -23,18 +22,20 @@ class ProcessarLinhasDetalhesCnab
 
             $titulo->banco = substr($detalhe, 0, 3);
             $titulo->valor_titulo = 200;
-            $titulo->valor_presente = $this->calcVp($titulo);
             $titulo->taxa_cessao = 1.50;
+            $titulo->valor_presente = $this->calcVp($titulo);
             $titulo->conteudo = $detalhe;
 
-            $dataInsert[] = $titulo->toArray();
+            $titulos[] = $titulo->toArray();
         }
 
-        DB::table("titulos")->insert($titulos);
+        foreach(array_chunk($titulos, 2000) as $chunk) {
+            DB::table("titulos")->insert($chunk);
+        }
     }
 
     private function calcVp(TituloDTO $tituloDTO): float
     {
-        return $tituloDTO->valor_titulo * pow(1 + ($tituloDTO->taxa_cessao / 100), 40 / 252);
+        return $tituloDTO->valor_titulo * pow(1 + ($tituloDTO->taxa_cessao / 100), 35 / 252);
     }
 }
